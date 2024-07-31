1e1b5d34cfd81046fad237668154fe28dd0d89d9
ravi ranjan <ravi848101mnb@gmail.com>
2024-07-31 23:32:08 +0530

    feat: Add JWT_SECRET to environment variables
    This commit adds the `JWT_SECRET` environment variable to the `confo.js` configuration file. The `JWT_SECRET` is used for signing and verifying JSON Web Tokens (JWTs) in the application. By including this variable in the configuration, the application can securely handle authentication and authorization using JWTs.

--- src/config/confo.js
+++ src/config/confo.js
@@ -4,6 +4,7 @@ const Env = {
     PORT: process.env.PORT || 3000,
     MONGODB_URI: process.env.MONDODB_URI || 'mongodb://localhost:27017',
     CORS_ORIGIN: process.env.CORS_ORIGIN,
+    JWT_SECRET: process.env.JWT_SECRET,
 };
 
 export default Env;
